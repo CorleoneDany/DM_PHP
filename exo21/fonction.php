9cad94de3f7a536253118dc0226620dea874226a
Merwan Daghou <merwan.daghou@yahoo.com>
2019-05-25 13:31:04 +0200

    Correction des erreurs de l'exo 21# Please enter the commit message for your changes. Lines starting

--- exo21/fonction.php
+++ exo21/fonction.php
@@ -1,30 +1,55 @@
 <?php 
 function recherche($mot)
 {
-	$lenght = strlen($mot);
+	$lenght = strlen($mot)+2; //longueur du mot + longueur de la chaîne "/n"
+	var_dump($lenght);
 	$dico=fopen('liste_francais.txt','r');
+	var_dump($mot);
 	$liste="";
-	$i=1;
+	
+	// Debug
+		// $ligne=fgets($dico);
+		// if($lenght==strlen($ligne)){
+		// echo $ligne;
+		// $i = 0;
+		// var_dump($i);
+		// while($i<=$lenght-3){
+			// echo $i;
+			// echo $mot{$i};
+			// echo $ligne{$i};
+			// $i+=1;
+		// }
+	
+		// }
+		
+	//Fin debug
+	
+	
+	
 	While(!feof($dico))
 	{
-		//--------- Lit la prochaine ligne
+//		--------- Lit la prochaine ligne
 		$ligne=fgets($dico);
 		$bIdentique=true;
-		//--------- Si les mots font la même taille
+		// echo $ligne." ".$bIdentique ;
+//		--------- Si les mots font la même taille
 		if($lenght==strlen($ligne))
 		{
-			while($i<=$lenght && $bIdentique==True)
+			$i=0;
+			while($i<=$lenght-3 && $bIdentique==true)
 			{
 				if($mot{$i}!="?")
 					{
-					if($mot{$i}!=$ligne{$i})
+					if($mot[$i]!=$ligne{$i}){
 						$bIdentique=false;
+						}
 					}
+				$i=$i+1;
 			}
 			
 			if($bIdentique==true)
 				{
-					$liste=$liste.$ligne." ";
+					$liste.=$ligne." ";
 				}
 		}	
 	}
