07b2b0d7a364ea82504c1f5a3af78b0e31ee728f
DanyCorleone <corleonedany@live.fr>
2019-05-26 17:25:23 +0200

    Tentative de compréhension du foreach sur tableau multidimensionnel

--- Exo13/index.php
+++ Exo13/index.php
@@ -1,20 +1,32 @@
-<?php
-$produit1 = array("numero"=>1, "nom"=>"Montre", "prix"=>"100 euros", "descriptif"=>"Une jolie montre."); 
-$produit2 = array("numero"=>2, "nom"=>"Souris", "prix"=>"10 euros", "descriptif"=>"Une jolie souris."); 
-$produit3 = array("numero"=>3, "nom"=>"Clavier", "prix"=>"20 euros", "descriptif"=>"Un joli clavier."); 
-$produits = array($produit1, $produit2, $produit3);
+<!-- <!DOCTYPE html>
+<html lang="fr">
 
-function genererListeProduits()
-{
-// $produit1 = array("numero"=>1, "nom"=>"Montre", "prix"=>"100 euros", "descriptif"=>"Une jolie montre."); 
-// $produit2 = array("numero"=>2, "nom"=>"Souris", "prix"=>"10 euros", "descriptif"=>"Une jolie souris."); 
-// $produit3 = array("numero"=>3, "nom"=>"Clavier", "prix"=>"20 euros", "descriptif"=>"Un joli clavier."); 
-// $produits = array($produit1, $produit2, $produit3);
-    foreach($produit1 as $k=>$v)
+<head>
+    <meta charset="UTF-8">
+    <title>Exo 13</title>
+</head>
+
+<body>
+
+    <?php
+    function genererFicheProduits()
     {
-        echo $k. "=>" . $v;
+        
     }
-}
+    function genererListeProduits()
+    {
+        $produit1 = array("numero" => 1, "nom" => "Montre", "prix" => "100 euros", "descriptif" => "Une jolie montre.");
+        $produit2 = array("numero" => 2, "nom" => "Souris", "prix" => "10 euros", "descriptif" => "Une jolie souris.");
+        $produit3 = array("numero" => 3, "nom" => "Clavier", "prix" => "20 euros", "descriptif" => "Un joli clavier.");
+        $produits = array($produit1, $produit2, $produit3);
 
-genererListeProduits();
-?>
+        foreach ($produits as $k => $v)
+        {
+            echo $k . " => " . $v . "<br/>";
+        }
+    }
+
+    genererListeProduits(); ?>
+</body>
+
+</html> -->
