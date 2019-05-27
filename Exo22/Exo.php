a5599946a3a8a29cf33a2259271b2312231cb547
Dany Corleone <corleonedany@live.fr>
2019-05-27 11:16:21 +0200

    Suppression des variables Couleur et Taille, le code passe désormais par la variable  pour récupérer les fichiers css

--- Exo22/Exo.php
+++ Exo22/Exo.php
@@ -4,15 +4,13 @@ define('CSS_PATH', './css/');
 
 if(!empty($_POST))
 {
-$Couleur = $_POST["Couleur"];
-$Taille = $_POST["Taille"];
 setcookie("Couleur", $_POST["Couleur"]);
 setcookie("Taille", $_POST['Taille']);
 }
 ?>
 
-<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH . $Couleur?> ">
-<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH . $Taille?> ">
+<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH . $_COOKIE['Couleur']?> ">
+<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH . $_COOKIE['Taille']?> ">
 <form action="Exo.php" method="post">
 <label>Couleur</label><br>
 <select name="Couleur" size="1">
@@ -29,7 +27,6 @@ setcookie("Taille", $_POST['Taille']);
 </select><br><br>
 
 <input type="submit" value="Sauvegarder"><br><br>
-
-<?php echo "<p>Visiblement, vous aimez le " . $Couleur . "</p>"; ?>
+<a href="./Texte.php"> Test des préférences</a>
 </form>
 </html>
---
+++ Exo22/Texte.php
@@ -0,0 +1,21 @@
+<?php
+define('CSS_PATH', './css/');
+?>
+
+<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH . $_COOKIE['Couleur']?> ">
+<link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH . $_COOKIE['Taille']?> ">
+
+<p>
+Lorem ipsum dolor sit amet,
+ consectetur adipiscing elit.
+  Pellentesque efficitur libero orci,
+   at sollicitudin lacus finibus at.
+    Nam nec gravida magna, eget auctor libero.
+     Curabitur blandit pharetra blandit.
+      Morbi pellentesque eget leo id faucibus.
+       Ut pretium porttitor dignissim.
+        Nunc fermentum felis sed est accumsan fermentum.
+         Praesent maximus bibendum diam non ullamcorper.
+          Maecenas mattis justo tincidunt commodo condimentum.
+           In posuere lobortis purus, et commodo urna fermentum a.
+            Cras ornare porttitor feugiat.</p>
