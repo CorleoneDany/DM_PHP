e02bb6462937af36a0b23be68fb840463cfe183f
Dany Corleone <corleonedany@live.fr>
2019-05-27 15:51:21 +0200

    Ajout des PK

--- Partie_2/Script.sql
+++ Partie_2/Script.sql
@@ -1,18 +1,22 @@
 CREATE DATABASE bibliotheque;
 CREATE TABLE abonne(
     id_abonne INT(3),
-    prenom VARCHAR(25)
+    prenom VARCHAR(25),
+    CONSTRAINT PK_abonne PRIMARY KEY (id_abonne)
 );
+
 CREATE TABLE emprunt(
     id_emprunt INT(3),
     id_livre INT(3),
     id_abonne INT(3),
     date_sortie DATE,
-    date_rendu DATE
-)
+    date_rendu DATE DEFAULT NULL
+    CONSTRAINT PK_emprunt PRIMARY KEY (id_emprunt)
+);
 
 CREATE TABLE livre(
     id_livre INT(3),
     auteur VARCHAR(25),
     titre VARCHAR(50)
-)
+    CONSTRAINT PK_livre PRIMARY KEY (id_livre)
+);
