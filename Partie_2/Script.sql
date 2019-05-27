c09f2038bc871ea69c3dcf2f3fdc9249e67f7440
Dany Corleone <corleonedany@live.fr>
2019-05-27 16:41:50 +0200

    Creation de la structure finie

--- Partie_2/Script.sql
+++ Partie_2/Script.sql
@@ -1,24 +1,25 @@
-CREATE DATABASE bibliotheque;
+CREATE DATABASE IF NOT EXISTS bibliotheque;
+USE bibliotheque;
 CREATE TABLE abonne(
-    id_abonne INT(3),
-    prenom VARCHAR(25),
-    CONSTRAINT PK_abonne PRIMARY KEY (id_abonne)
+  id_abonne INT(3) AUTO_INCREMENT,
+  prenom VARCHAR(25),
+  CONSTRAINT PK_abonne PRIMARY KEY (id_abonne)
 );
 
 CREATE TABLE emprunt(
-    id_emprunt INT(3),
-    id_livre INT(3),
-    id_abonne INT(3),
-    date_sortie DATE,
-    date_rendu DATE DEFAULT NULL,
-    CONSTRAINT PK_emprunt PRIMARY KEY (id_emprunt),
-    CONSTRAINT FK_livre FOREIGN KEY (id_livre) REFERENCES livre(id_livre),
-    CONSTRAINT FK_abonne FOREIGN KEY (id_abonne) REFERENCES abonne(id_abonne)
+  id_emprunt INT(3) AUTO_INCREMENT,
+  id_livre INT(3),
+  id_abonne INT(3),
+  date_sortie DATE,
+  date_rendu DATE DEFAULT NULL,
+  CONSTRAINT PK_emprunt PRIMARY KEY (id_emprunt),
+  CONSTRAINT FK_livre FOREIGN KEY (id_livre) REFERENCES livre(id_livre),
+  CONSTRAINT FK_abonne FOREIGN KEY (id_abonne) REFERENCES abonne(id_abonne)
 );
 
 CREATE TABLE livre(
-    id_livre INT(3),
-    auteur VARCHAR(25),
-    titre VARCHAR(50),
-    CONSTRAINT PK_livre PRIMARY KEY (id_livre)
+  id_livre INT(3) AUTO_INCREMENT,
+  auteur VARCHAR(25),
+  titre VARCHAR(50),
+  CONSTRAINT PK_livre PRIMARY KEY (id_livre)
 );
