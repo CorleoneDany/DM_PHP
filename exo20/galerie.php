3e3ed234a722b860f31c809c826e6435e1608654
Merwan Daghou <merwan.daghou@yahoo.com>
2019-05-30 15:21:20 +0200

    Update exo 20 + correction exo 17

--- exo20/galerie.php
+++ exo20/galerie.php
@@ -1,29 +1,42 @@
 <?php
-
 if(!empty($_FILES)){
-	if(!is_dir('image')){
-		mkdir('image');
+	$dossier = 'galerie';
+	if(!is_dir($dossier)){
+		mkdir($dossier);
 	}
 	$img = $_FILES['image'];
-	move_uploaded_file($img['tmp_name'],"image/".$img['name']);
+	$extension = substr($img['name'],-3);
+	$tabext = array('tif','jpg','gif','png');
+	$bBonFormat=false;
+	foreach($tabext as $v){
+		if($extension==$v){
+			$bBonFormat=true;
+			break;
+		}
+	}
+	
+	if($bBonFormat){
+		move_uploaded_file($img['tmp_name'],$dossier."/".$img['name']);
+	}
+	else{
+		echo "Le fichier sélectionné n'est pas au format d'image";
+	}
 }
-
-
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 	<head>
 		<title>DM PHP: Exercice 20</title>
 		<meta charset="utf-8">
 		<meta name="Author" content="Merwan DAGHOU">
 	</head>
 	<body>
-		<form method="get" action="galerie.php">
+		<form method="post" action="galerie.php" enctype="multipart/form-data">
 			<label for="image">Ajouter une image : </label>
-			<input type="text" name="nom" id="nom"/><br/>
 			<input type="file" name="image" id="image"/><br/>
-			<input type="submit" value="Ajouter l'image"/>
+			<input type="submit" value="Ajouter l'image"/><br/><br/>
 		</form>
+
 	</body>
-</html>
+</html> 
