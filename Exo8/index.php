554d579b5e181d40614355c51798fe62adfe3691
Dany Corleone <corleonedany@live.fr>
2019-05-31 16:10:23 +0200

    La fonction nextSuite est désormais fonctionnelle

--- Exo8/index.php
+++ Exo8/index.php
@@ -1,30 +1,34 @@
 <?php
 
 function nextSuite()
-{   $str = '1';
+{
+    $str = '1';
     $nextstr = '';
 
     for ($i = 0; $i < strlen($str); $i++)
     {
-        if (substr($str, $i) != substr($str, $i+1)) //Un caractère identique
+        if (substr($str, $i) != substr($str, $i + 1)) 
         {
+            //Un caractère identique
             $nextstr .= '1' . substr($str, $i);
         }
         else
-                {
-                    if (substr($str, $i) != substr($str, $i+2)) //Deux caractères identiques
-                    {
-                        $nextstr .= '2' . substr($str, $i);
-                        $i++;
-                    }
-                    else
-                        {
-                            //Trois caractères identiques
-                            $nextstr .= '3' . substr($str, $i);
-                            $i += 2;
-                        }
-                }
+        {
+            if (substr($str, $i) != substr($str, $i + 2)) 
+            {
+                //Deux caractères identiques
+                $nextstr .= '2' . substr($str, $i);
+                $i++;
+            }
+            else
+            {
+                //Trois caractères identiques
+                $nextstr .= '3' . substr($str, $i);
+                $i += 2;
+            }
+        }
     }
+    echo $nextstr;
 }
 
 nextSuite();
